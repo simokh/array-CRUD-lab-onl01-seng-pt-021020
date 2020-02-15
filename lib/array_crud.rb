b684a034898e9fa3b07c945cc33312a3c73a903e
Simo Khouili <m.simokhouili@gmail.com>
2020-02-15 22:10:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,29 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my
+  my_array << 'so'
 end
 
 def add_element_to_start_of_array(array, element)
-
+  my_array.unshift('wo')
 end
 
 def remove_element_from_end_of_array(array)
-
+  my_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  my_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  my_array[2]
 end
 
 def retrieve_first_element_from_array(array)
-
+  my_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  my_array[3]
 end
